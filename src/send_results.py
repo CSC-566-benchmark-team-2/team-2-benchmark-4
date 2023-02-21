45ca297ea414230690307c7a23c5390050ab1b8c
waidhoferj <waidhoferj@gmail.com>
2023-02-21 13:12:48 -0800

    updated endpoint

--- src/send_results.py
+++ src/send_results.py
@@ -11,6 +11,9 @@ import json
 
 from dataclasses import dataclass, asdict
 
+ENDPOINT_URL = "https://csc-566-benchmarks-default-rtdb.firebaseio.com/benchmark-3.json"
+
+
 @dataclass
 class UserResults:
     username: str
@@ -20,21 +23,21 @@ class UserResults:
     def __init__(self, username: str = "unknown_user", datasets: dict = {}):
         self.username = username
         self.datasets = datasets
-        self.timestamp = 0.
+        self.timestamp = 0.0
         return
 
     def add_dataset(self, dataset_name: str, metrics: dict):
-        """ adds a dataset + metrics to user """
+        """adds a dataset + metrics to user"""
         self.datasets[dataset_name] = metrics
         return
 
     def add_metric_to_dataset(self, dataset_name: str, metric_name: str, value: float):
-        """ adds a metric to an existing dataset """
+        """adds a metric to an existing dataset"""
         self.datasets[dataset_name][metric_name] = value
         return
-    
+
     def get_metrics(self):
-        """ returns a list of metrics that are used in the datasets """
+        """returns a list of metrics that are used in the datasets"""
         metrics = []
         for dataset in self.datasets.keys():
             for metric in list(self.datasets[dataset].keys()):
@@ -42,8 +45,7 @@ class UserResults:
         return list(set(metrics))
 
     def send_results(self):
-        """ Sends a request containing user submission to firebase db """
-        ENDPOINT_URL = 'https://csc-566-benchmarks-default-rtdb.firebaseio.com/benchmark-2.json'
+        """Sends a request containing user submission to firebase db"""
 
         self.timestamp = time.time()
         resp = requests.post(ENDPOINT_URL, json=asdict(self))
@@ -53,22 +55,21 @@ class UserResults:
             return False
         return True
 
+
 def old_send_results(github_username, video_game_results, life_expectancy_results):
     """
-    This function sends our results to our database on Firebase. 
+    This function sends our results to our database on Firebase.
 
     Args:
         github_username (str): The string github username
-        video_game_results (tuple): This is a tuple of the results of the model on video game 
+        video_game_results (tuple): This is a tuple of the results of the model on video game
             data. It is in the format:
                 -> (Model Accuracy: float, Model Runtime: Int)
-        life_expectancy_results (tuple): This is a tuple of the results of the model on video game 
+        life_expectancy_results (tuple): This is a tuple of the results of the model on video game
             data. It is in the format:
                 -> (Model Accuracy: float, Model Runtime: Int)
-            
-    """
-    ENDPOINT_URL = 'https://csc-566-benchmarks-default-rtdb.firebaseio.com/benchmark-2.json'
 
+    """
     # Parse results from tuple parameters
     vg_acc, vg_time = video_game_results
     le_acc, le_time = life_expectancy_results
@@ -92,29 +93,31 @@ def old_send_results(github_username, video_game_results, life_expectancy_result
     if resp.status_code != 200:
         print("Error - Issue with post request to backend :(")
 
-if __name__ == '__main__':
-    username='dummy_username_1'
+
+if __name__ == "__main__":
+    username = "dummy_username_1"
 
     video_game_model_accuracy = 0.33
     video_game_model_runtime = 15
     # video_game_results = (video_game_model_accuracy, video_game_model_runtime)
-    video_game_metrics = {"accuracy": video_game_model_accuracy, "runtime": video_game_model_runtime}
+    video_game_metrics = {
+        "accuracy": video_game_model_accuracy,
+        "runtime": video_game_model_runtime,
+    }
 
     life_expectancy_model_accuracy = 0.76
     life_expectancy_model_runtime = 8
     # life_expectancy_results = (life_expectancy_model_accuracy, life_expectancy_model_runtime)
-    life_expectancy_metrics = {"accuracy": life_expectancy_model_accuracy, "runtime": life_expectancy_model_runtime}
+    life_expectancy_metrics = {
+        "accuracy": life_expectancy_model_accuracy,
+        "runtime": life_expectancy_model_runtime,
+    }
 
     # old_send_results(username, video_game_results, life_expectancy_results)
     user_results = UserResults(
-        username=username,
-        datasets={
-            "Video Games": video_game_metrics
-        }
+        username=username, datasets={"Video Games": video_game_metrics}
     )
     user_results.add_dataset("Life Expectancy", life_expectancy_metrics)
     user_results.send_results()
 
     sys.exit(0)
-
-
